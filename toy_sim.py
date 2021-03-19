3bdde28505c9802c2d337ef31c9c06401d37b084
Gareth Hillston <gareth.hillston@gmail.com>
2021-03-19 11:06:54 +0000

    Toy model working!

--- toy_sim.py
+++ toy_sim.py
@@ -1,46 +1,39 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib import colors
+from datetime import datetime
+import os
 
 gridSize = 50
-maxIterations = 200
-numParticles = 50000
-ticksPerFrame = 2
-zmax = 300
+maxIterations = 10
+numParticles = 100
+
+now = datetime.now().strftime("%d_%m_%y__%H%M%S")
+outputFolder = "./plots/{dateTime}".format(dateTime=now)
+os.mkdir(outputFolder)
 
 fig = plt.figure()
 ax = fig.add_subplot(111)
 
-# meshgrid setup
-x = y = np.linspace(1,gridSize,gridSize)
-X, Y = np.meshgrid(x, y)
-
 # colour map
-vmin, vmax = 0, zmax
-norm = colors.Normalize(vmin=vmin, vmax=vmax)
+colours = np.random.randint(0, numParticles, size=(numParticles))
 
 # Initialise all particles to the centre
 locations = np.ones((numParticles, 2), dtype=int) * gridSize//2
 
-for j in range(maxIterations):
+for i in range(maxIterations):
     # randomly move particles
     locations += np.random.randint(-1, 2, locations.shape)
 
-    if not (j+1) % ticksPerFrame:
-        # Create and update new grid
-        grid = np.zeros((gridSize, gridSize))
+    for j in range(numParticles):
+        x, y = locations[j]
 
-        for i in range(numParticles):
-            x, y = locations[i]
-            # check if particle is still on grid, then add it
-            if ( 0 <= x < gridSize and 0 <= y < gridSize):
-                grid[x, y] += 1
+    print(i + 1, '/', maxIterations)
 
-        print(j+1, '/', maxIterations)
+    transLocs = locations.transpose()
 
-        # clear previous plot and replot
-        ax.clear()
-        ax.scatter(X, Y, grid)
+    # clear previous plot and replot
+    ax.clear()
+    ax.scatter(transLocs[0], transLocs[1], c=colours, alpha=0.5)
 
-        #save to files
-        plt.savefig('plots/sim-{:03d}.png'.format(j//ticksPerFrame))
+    plt.savefig('{outputFolder}/sim-{iter:03d}.png'.format(outputFolder=outputFolder, iter=i))
+    # plt.show()
